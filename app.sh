2da88a002026e5efe42cd9bd4bb40cfbd79338dc
ddr <ddr819331062@163.com>
2024-07-29 10:13:20 +0800

    启动脚本添加内存设置和垃圾回收日志

--- app.sh
+++ app.sh
@@ -13,10 +13,17 @@ APP_JAR="$APP_NAME.jar"
 # 定义Spring Boot应用的PID文件
 PID_FILE="application.pid"
 
+# Spring Boot应用的内存设置
+MEM_OPTS="-Xms512m -Xmx1024m"
+
+# GC日志的输出路径和文件名
+GC_LOG_FILE=./gc.log
+GC_LOG_OPTS="-Xloggc:$GC_LOG_FILE -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M"
+
 # 定义启动Spring Boot应用的命令
 start() {
     echo "Starting $APP_NAME..."
-    nohup java -jar $APP_JAR > /dev/null 2>&1 &
+    nohup java $MEM_OPTS $GC_LOG_OPTS -jar $APP_JAR > /dev/null 2>&1 &
     echo $! > $PID_FILE
     echo "$APP_NAME started successfully."
 }
